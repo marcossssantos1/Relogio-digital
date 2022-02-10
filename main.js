8bda0d250d5116d571148b5b81ae6c022d178967
marcossantos1 <marcossantosmsms604@gmail.com>
2022-02-10 15:21:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
-function relogio(){
-        var data = new Date();
-        var hr = data.getHours();
-        var min = data.getMinutes();
-        var seg = data.getSeconds();
+function ola(){
+      var txt = "Obrigado por acessar minha pagina!";
+      alert(" Ola, " + txt);
+    }
+ola();
+setInterval(setDate, 100)
 
-        if(hr <10){
-          hr="0"+hr;
-        }
-         if(min <10){
-          min="0"+min;
-        }
-         if(seg <10){
-          seg="0"+seg;
-        }
+const hor = document.querySelector("[data-hour-hand]");
+const min = document.querySelector("[data-minutes-hand]");
+const seg = document.querySelector("[data-seconds-hand]");
 
-        var horas = hr + " : " + min + " : " + seg;
-        document.getElementById("rel").value=horas;
-      } var tempo = setInterval (relogio,1000);
+function setDate(){
+  const corrDate = new Date();
+  const segundos = corrDate.getSeconds() / 60;
+  const minutos = (segundos + corrDate.getMinutes()) / 60;
+  const horas = (minutos + corrDate.getHours()) / 12;
+  setRotation (seg, segundos);
+  setRotation (min, minutos);
+  setRotation (hor, horas);
+}
+
+function setRotation(element, rotationRatio){
+  element.style.setProperty("--rotation", rotationRatio *360);
+}
+
+setDate();
+
+var dt = document.getElementById("#data");
+   dt = new Date();
+
+    dt.setFullYear(dt.getFullYear()); 
+
+    var dia = dt.getDay();
+    var mes = dt.getMonth();
+    var ye = dt.getFullYear();
+
+
+    var meses = ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"];
+    var semana = ["Dom","Seg","Ter","Qua","Qui","Sex","Sab"];
+
+   document.write(semana[dia] + " , "+meses[mes] + " de " + ye + "<br>");
+
